f09b3394273ff03fc53219bdc079802188281c63
SirGhock <joaofernandogarciadesouza@hotmail.com>
2019-10-29 02:19:33 -0300

    add friends and follow/unfollow

--- controller/UserController.js
+++ controller/UserController.js
@@ -29,6 +29,30 @@ exports.create_user = function (req, res) {
 };
 
 
+exports.list_friends = function (req, res) {
+    if (req.session && req.session.key) {
+        userModel.findOne({ "usuario": req.session.key }, function (err, doc) {
+            if (err || doc === null) {
+                return res.send('[' + JSON.stringify({ message: "Erro no BD" }) + ']');
+            }
+
+            amigos({ _id: { $in: doc.friends } }).then(function (listaAmigos) {
+                res.render('friends', {logado: req.session.key, amigos: listaAmigos });
+            });
+        });
+
+    }
+    else {
+        res.redirect('/');
+    }
+};
+
+const amigos = async function (params) {
+    try {
+        return await userModel.find(params)
+    } catch (err) { console.log(err) }
+}
+
 exports.login = function (req, res) {
     let usuario = req.body.usuario, senha = req.body.senha;
 
@@ -55,3 +79,50 @@ exports.sair = function (req, res) {
     return res.redirect('/');
 };
 
+exports.buscar = function (req, res) {
+    let search = req.query.q;
+    userModel.find({ usuario: { $not: { $regex: req.session.key }, $regex: '.*' + search + '.*', $options: 'i' } }, function (err, doc) {
+        if (err) console.log(err);
+        
+        if (doc == undefined) {
+            return res.send('<p style="color:#fff;">Nada encontrado</p>');
+        }
+
+        res.render('search', { resultados: doc, layout: 'search' });
+    }).limit(10);
+};
+
+exports.follow = function (req, res) {
+    let id = req.query.id;
+    if (req.session && req.session.key) {
+
+        userModel.findOne({ "usuario": req.session.key }, function (err, doc) {
+            if (err || doc === null) {
+                return res.send('[' + JSON.stringify({ message: "Erro no BD" }) + ']');
+            }
+
+            doc.friends.push({ _id: id });
+
+            doc.save(function () {
+                res.send('<p style="color:#fff;">Você agora segue essa pessoa</p>');
+            });
+        });
+    }
+    else {
+        res.redirect('/');
+    }
+}
+
+exports.unfollow = function(req, res){
+    var usuario = req.session.key, unFriend = req.query.id;
+    if(unFriend == null){
+        return res.redirect('/');
+    }
+    userModel.findOne({usuario: usuario}, function(err, doc){
+
+        doc.friends.pull({ _id: unFriend });
+        doc.save(function () {
+            res.send('<p style="color:#fff;">Você não segue mais essa pessoa</p>');
+        });
+    });
+ };
